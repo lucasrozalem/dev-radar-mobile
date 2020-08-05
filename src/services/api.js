ca9c76a9eeebe7cb9093087e7800d6c6c37e17d5
Lucas Rozalem <lrozalem@gmail.com>
2020-08-05 14:41:33 -0300

    finalized

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+  //baseURL: 'http://192.168.15.46:3333',
+  //baseURL: 'http://192.168.15.69:3333',
+  baseURL: 'http://10.0.2.2:3333',
+});
+
+export default api;
